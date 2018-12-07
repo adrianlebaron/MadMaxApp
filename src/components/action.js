77ce04d41b1cf8844bcad53a68f6d15c3ef72e65
A.D <adrianlebaron@gmail.com>
2018-12-07 16:15:18 -0700

    used react animate height  library to animate paragraph height

--- src/components/action.js
+++ src/components/action.js
@@ -2,28 +2,40 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
-  handleAction = function() {
-    this.props.onClick()
 
-    // document.getElementById('action').classList.add('')
-}.bind(this);
+    constructor(props) {
+        super(props)
 
-render() {
-    return (
-      <a
-                 id='action'
-                 onClick={() => this.handleAction()}
-                 className={`${this.props.className} action`}>
-             
-             </a>
-         )
-         // return (
-         //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-         //         { Icon('fas fa-plus-circle') }
-         //         {/* fas fa-times-circle */}
-         //     </button>
-         // )
-  }
+        this.status = false;
+    }
+
+    handleAction = function() {
+        this.props.onClick()
+        if(!this.status) {
+            document.getElementById(this.id).classList.add('action-remove');
+        } else {
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;
+    }.bind(this);
+
+    render() {
+        this.id = `action ${this.props.id}`
+        return (
+            <a
+                id={this.id}
+                onClick={() => this.handleAction()}
+                className={`${this.props.className} action`}>
+            
+            </a>
+        )
+        // return (
+        //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+        //         { Icon('fas fa-plus-circle') }
+        //         {/* fas fa-times-circle */}
+        //     </button>
+        // )
+    }
 }
 
 export default Action;
